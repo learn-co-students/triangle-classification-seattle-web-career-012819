32864e0511d188035c4f8e283b6cf94ffc213fa5
github username <github email address>
2019-02-10 17:06:17 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  
+  def initialize(*sides)
+    if sides.length != 3
+      raise TriangleError
+    else
+      @left, @right, @hyp = sides.sort {|a,b| a <=> b }
+    end
+  end
+
+  def kind
+    if @left + @right <= @hyp
+      raise TriangleError
+    elsif @left == 0 || @right == 0 || @hyp == 0
+      raise TriangleError
+    end
+
+    if @left == @right && @right == @hyp
+      :equilateral
+    elsif @left == @right || @left == @hyp || @hyp == @right
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError; end
 end
